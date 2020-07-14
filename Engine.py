2b56a845ec24369052e1ab41933e40018a8f629d
Edgar Andreé Toledo Torres <edgar.andree20@gmail.com>
2020-07-13 18:30:31 -0600

    Color edits

--- Engine.py
+++ Engine.py
@@ -2,11 +2,11 @@ from gl import Render
 from gl import color
 
 bmpFile = Render()
-bmpFile.glCreateWindow(320, 180) # Screen size
+bmpFile.glCreateWindow(800, 600) # Screen size
 bmpFile.glViewport(20, 20, 50 , 150) 
 bmpFile.glClear()
 bmpFile.glClearColor(0, 0, 0) # Black
-bmpFile.glColor(0.53, 0.80, 0.92) # Sky blue
+bmpFile.glColor(0.53, 0.80, 0.92) # Added random color
 bmpFile.glVertex(-1, -1) # Point on left lower corner
 bmpFile.glVertex(0, 0) # Point on center
 bmpFile.glVertex(1, 1) # Point on right upper corner
